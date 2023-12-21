5d16c16daf53f46ff5cdbb3dc2d0253cff413f29
AmrMustafa <amrdarwish115@gmail.com>
2023-12-22 01:29:54 +0200

    .env

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
+const dotenv = require("dotenv");
 const fs = require('fs')
 const cors = require('cors')
 const subjects = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`, "utf-8"));
-
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 app.use(cors())
 
 // console.log(subjects);
 
-app.listen(3000,
+app.listen(process.env.PORT,
 console.log('app working on port 3000')
 )
 
